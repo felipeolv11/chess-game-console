ab43cb1015c0ee2ab05b3e909b89268ce5824439
Felipe <felipeeh1524@gmail.com>
2026-08-27 13:52:40 -0300

    commit inicial

---
+++ chess game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace chess_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
