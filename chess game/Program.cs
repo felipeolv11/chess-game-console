43ea39940e61a651a832d804e75ff948bde665d9
Felipe <felipeeh1524@gmail.com>
2026-08-31 19:40:53 -0300

    controlar as peças capturadas

--- chess game/Program.cs
+++ chess game/Program.cs
@@ -16,10 +16,7 @@ namespace chess_game
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Jogador atual: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
